2f0992d5edde46ab4d91e5760ac124bfa4579941
Unlimited <35296052+AMSDE@users.noreply.github.com>
2018-06-23 21:50:24 +0800

    Zq

---
+++ js/tags.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+	$("taguse").html("<div class='aside-widget'><header><h3>标签</h3></header><div class='body clearfix'><ul class='tags'><li><a href='/blogs/DP/index.html'>DP</a></li></ul></div></div>");
+});
